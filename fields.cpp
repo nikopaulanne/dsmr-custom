0b18fd6a7c5064ff96e073f84a717e6e9d396421
nikopaulanne <niko.mj@paulanne.com>
2025-06-07 17:29:47 +0300

    Update fields.cpp

--- fields.cpp
+++ fields.cpp
@@ -1,19 +1,49 @@
+/*
+ * This file is part of the dsmr_custom ESPHome component.
+ *
+ * Copyright (c) 2025 (Niko Paulanne)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+/*
+ * Arduino DSMR parser.
+ *
+ * This software is licensed under the MIT License.
+ *
+ * Copyright (c) 2015 Matthijs Kooijman <matthijs@stdin.nl>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 /**
  * @file fields.cpp
  * @brief Vendored and modified field definitions source from glmnet/arduino-dsmr.
- * @details This file provides the storage definitions for static constexpr members
- * (OBIS IDs and PROGMEM names) declared in the corresponding `fields.h` for
- * various DSMR P1 telegram data fields. It has been modified for the
- * ESPHome dsmr_custom component to align with changes in `DEFINE_FIELD` macro.
- *
- * @note This file is part of a vendored copy of the glmnet/arduino-dsmr parser,
- * which itself is based on matthijskooijman/arduino-dsmr.
- * Original library (glmnet): https://github.com/glmnet/arduino-dsmr
- * It has been included locally in the ESPHome dsmr_custom component.
- * Modifications are marked and explained.
- *
- * Original Copyright (c) 2015 Matthijs Kooijman <matthijs@stdin.nl>
- * Licensed under the MIT License. (Assumed, as per base library)
+ * @details Provides storage definitions for static members declared in fields.h.
+ * Modified for the ESPHome dsmr_custom component to align with the updated
+ * DEFINE_FIELD macro.
+ * @author Niko Paulanne
  */
 
 // BEGIN MODIFICATION FOR ESPHOME DSMR_CUSTOM
