8c8c95cba364972283c59c8e6de67dffb6162bf1
nikopaulanne <niko.mj@paulanne.com>
2025-06-07 17:52:54 +0300

    Update text_sensor.py

--- text_sensor.py
+++ text_sensor.py
@@ -1,14 +1,23 @@
 #
 # This file is part of the dsmr_custom ESPHome component.
 #
-# Copyright (c) 2025 (Niko Paulanne)
+# This file is inspired by or based on the original ESPHome DSMR component,
+# available at: https://github.com/esphome/esphome/tree/dev/esphome/components/dsmr
+#
+# Modifications and new code are Copyright (c) 2025 (Niko Paulanne).
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 ESPHome platform for standard text-based DSMR sensors using the dsmr_custom hub.
