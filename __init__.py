5c9ade5a2f97eab775f6200dc1c211716a308755
nikopaulanne <niko.mj@paulanne.com>
2025-06-07 17:29:14 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,23 @@
-"""
-ESPHome custom component for DSMR P1 port reading with advanced customization.
-Aligned with native ESPHome DSMR component practices where applicable.
-"""
+#
+# This file is part of the dsmr_custom ESPHome component.
+#
+# Copyright (c) 2025 (Niko Paulanne)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""dsmr_custom component for ESPHome."""
 
 from pathlib import Path
 from esphome import pins
