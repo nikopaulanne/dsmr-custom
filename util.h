c99e471978b3eda87071b229bb9a33805b40d0bb
nikopaulanne <niko.mj@paulanne.com>
2025-06-07 16:33:25 +0300

    Update util.h

--- util.h
+++ util.h
@@ -1,12 +1,57 @@
+/*
+ * This file is part of the dsmr_custom ESPHome component.
+ *
+ * Copyright (c) 2025 (Niko Paulanne)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Arduino DSMR parser.
+ *
+ * This software is licensed under the MIT License.
+ *
+ * Copyright (c) 2015 Matthijs Kooijman <matthijs@stdin.nl>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 /**
  * @file util.h
  * @brief Vendored and modified utility functions and structures from glmnet/arduino-dsmr.
- * @details Provides ObisId, ParseResult (with members like result_, next_, err_, ctx_, v_),
- * concat_hack, and DSMR_PROGMEM. Adapted for ESPHome dsmr_custom component.
- *
- * @note Vendored from glmnet/arduino-dsmr, based on matthijskooijman/arduino-dsmr.
- * Modifications: Unique include guard, ESPHome style for member names (trailing underscores),
- * refined DSMR_PROGMEM.
+ * @details Provides ObisId, ParseResult, and other helpers. This version has been
+ * modified for dsmr_custom to use trailing underscores for member variables for
+ * ESPHome C++ style compliance.
+ * @author Niko Paulanne
  */
 
 // BEGIN MODIFICATION FOR ESPHOME DSMR_CUSTOM
